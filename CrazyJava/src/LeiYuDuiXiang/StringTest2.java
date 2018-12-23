868ac155029d571a7247af19f7b420d266904c17
ChengBin <cb514057880@163.com>
2018-12-24 03:09:10 +0800

    Revert "Revert "11.24""
    This reverts commit afb0b42c3c8318683ef6c025a3929bb1253c303e.

---
+++ CrazyJava/src/LeiYuDuiXiang/StringTest2.java
@@ -0,0 +1,18 @@
+package LeiYuDuiXiang;
+
+/**
+ * 1 *
+ * 2 * @Author: ChengBin
+ * 3 * @Date: 2018/12/24 02:12
+ * 4 * @
+ * 5 *
+ **/
+public class StringTest2 {
+    public static void main(String[] args) {
+        for (int i = 1;i<101;i++){
+            if(i!=100){
+                System.out.println('+');
+            }
+        }
+    }
+}
---
+++ CrazyJava/src/LeiYuDuiXiang/StringTest3.java
@@ -0,0 +1,18 @@
+package LeiYuDuiXiang;
+
+/**
+ * 1 *
+ * 2 * @Author: ChengBin
+ * 3 * @Date: 2018/12/24 02:14
+ * 4 * @
+ * 5 *
+ **/
+public class StringTest3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            //解决输出问题，之前也出现过
+            //System.out.println("%d+", i);
+        }
+        System.out.println(100);
+    }
+}
